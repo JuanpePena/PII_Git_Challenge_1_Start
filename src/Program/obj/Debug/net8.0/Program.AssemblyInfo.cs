20c5a4009459c55e6537a98cf86376d61e92be82
JuanpePena <juanpedro.pena@correo.ucu.edu.uy>
2024-08-07 10:27:50 -0300

    Implementacion de la operacion de resta

--- src/Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ src/Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c04646ab5601bfc1412781b2ddce377195b52f58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525bd2366c67f5aacada882351cb883f4d8138cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
